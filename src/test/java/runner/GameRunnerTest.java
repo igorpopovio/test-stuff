b2ff03db37d4842475ac8d0d0b8a5531583b2ef2
Igor Popov <igor.popov89@gmail.com>
2012-12-19 10:50:02 +0200

    Check actual output with golden master
    Signed-off-by: Igor Popov <igor.popov89@gmail.com>

--- src/test/java/runner/GameRunnerTest.java
+++ src/test/java/runner/GameRunnerTest.java
@@ -1,16 +1,21 @@
 package runner;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.Random;
+import java.util.Scanner;
 
+import static org.junit.Assert.assertEquals;
 import static runner.GameRunner.createGame;
 
 public class GameRunnerTest {
     private static final String GOLDEN_MASTER_DIR_NAME = "golden-master";
+    private static final long DEFAULT_SEED = 100;
 
     void deleteRecursivelyAllFilesFrom(File parent) {
         if (parent.isDirectory())
@@ -36,12 +41,33 @@ public class GameRunnerTest {
         return String.format("%s/%d.txt", GOLDEN_MASTER_DIR_NAME, seed);
     }
 
+    private void redirectConsoleOutputTo(ByteArrayOutputStream stream) {
+        System.setOut(new PrintStream(stream));
+    }
+
+    private String readFromGoldenMasterFor(long seed) throws FileNotFoundException {
+        return new Scanner(new File(getFileNameFor(seed))).useDelimiter("\\A").next();
+    }
+
     @Test
+    @Ignore
     public void shouldGenerateGoldenMaster() throws Exception {
         recreateTheGoldenMasterDirectory();
 
         long seed = 100;
         saveConsoleOutputInFile(new File(getFileNameFor(seed)));
         new GameRunner(createGame(), new Random(seed)).run();
+    }
+
+    @Test
+    public void shouldCheckActualOutputWithGoldenMaster() throws Exception {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        redirectConsoleOutputTo(stream);
+
+        new GameRunner(createGame(), new Random(DEFAULT_SEED)).run();
+
+        String expectedOutput = readFromGoldenMasterFor(DEFAULT_SEED);
+        String actualOutput = stream.toString();
+        assertEquals(expectedOutput, actualOutput);
     }
 }
