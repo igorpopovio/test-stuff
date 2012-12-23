3b9e1c1778ec32144c8a144401da04308658578b
Igor Popov <igor.popov89@gmail.com>
2012-12-23 19:38:05 +0200

    Rename instance variable in GeneratedQuestionBoardFactory
    Signed-off-by: Igor Popov <igor.popov89@gmail.com>

--- src/main/java/trivia/GeneratedQuestionBoardFactory.java
+++ src/main/java/trivia/GeneratedQuestionBoardFactory.java
@@ -1,23 +1,24 @@
 package trivia;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class GeneratedQuestionBoardFactory implements QuestionBoardFactory {
     private List<String> categories;
-    private int numberOfQuestions;
+    private int questionsPerCategory;
 
     public GeneratedQuestionBoardFactory(
             List<String> categories,
-            int numberOfQuestions) {
+            int questionsPerCategory) {
         this.categories = categories;
-        this.numberOfQuestions = numberOfQuestions;
+        this.questionsPerCategory = questionsPerCategory;
     }
 
     @Override
     public QuestionBoard createBoard() {
         DefaultQuestionBoard board = new DefaultQuestionBoard();
-        int timesToIterate = numberOfQuestions * categories.size();
-        CircularIterator<String> categoryIterator = new CircularIterator<>(categories);
+        int timesToIterate = questionsPerCategory * categories.size();
+        Iterator<String> categoryIterator = new CircularIterator<>(categories);
         for (int i = 0; i < timesToIterate; i++) {
             int questionIndex = i;
             for (int j = 0; j < categories.size(); j++) {
