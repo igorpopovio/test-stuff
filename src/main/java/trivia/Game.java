35788040687e99498ef6d62713c249e9a891dcb2
Igor Popov <igor.popov89@gmail.com>
2012-12-24 16:24:00 +0200

    Use CircularIterator to advance through players
    Signed-off-by: Igor Popov <igor.popov89@gmail.com>

--- src/main/java/trivia/Game.java
+++ src/main/java/trivia/Game.java
@@ -5,15 +5,13 @@ import java.util.List;
 import static trivia.Logger.log;
 
 public class Game {
-    int currentPlayer = -1;
-    List<Player> players;
-    private List<String> categories;
+    private CircularIterator<Player> players;
+    private Player currentPlayer;
     private Board board;
 
     public Game(List<String> categories, List<Player> players) {
-        this.categories = categories;
-        this.players = players;
         logAddedPlayers(players);
+        this.players = new CircularIterator<>(players);
         this.board = new Board(categories, players);
     }
 
@@ -26,45 +24,40 @@ public class Game {
 
     public void roll(int roll) {
         log("They have rolled a %d", roll);
-        if (currentPlayer().isInPenaltyBox()) doIfInPenaltyBox(roll);
+        if (currentPlayer.isInPenaltyBox()) doIfInPenaltyBox(roll);
         else doIfOutOfPenaltyBox(roll);
     }
 
     public void advanceToNextPlayer() {
-        currentPlayer++;
-        currentPlayer %= players.size();
-        log("%s is the current player", currentPlayer());
+        currentPlayer = players.next();
+        log("%s is the current player", currentPlayer);
     }
 
     public void provideCorrectAnswer() {
-        currentPlayer().provideCorrectAnswer();
+        currentPlayer.provideCorrectAnswer();
     }
 
     public void provideWrongAnswer() {
         log("Question was incorrectly answered");
-        currentPlayer().moveToPenaltyBox();
+        currentPlayer.moveToPenaltyBox();
     }
 
     public boolean isGameOver() {
-        return currentPlayer().getCoins() == 6;
+        return currentPlayer.getCoins() == 6;
     }
 
     public boolean isAllowedToAnswer() {
-        return currentPlayer().isAllowedToAnswer();
-    }
-
-    private Player currentPlayer() {
-        return players.get(currentPlayer);
+        return currentPlayer.isAllowedToAnswer();
     }
 
     private void doIfOutOfPenaltyBox(int roll) {
         updatePlace(roll);
-        log("%s's new location is %d", currentPlayer(), board.getCurrentPlaceFor(currentPlayer()));
+        log("%s's new location is %d", currentPlayer, board.getCurrentPlaceFor(currentPlayer));
         askQuestion();
     }
 
     private void updatePlace(int roll) {
-        board.updatePlace(currentPlayer(), roll);
+        board.updatePlace(currentPlayer, roll);
     }
 
     private void doIfInPenaltyBox(int roll) {
@@ -81,15 +74,15 @@ public class Game {
     }
 
     private void keepInPenaltyBox() {
-        currentPlayer().keepInPenaltyBox();
+        currentPlayer.keepInPenaltyBox();
     }
 
     private void moveOutOfPenaltyBox() {
-        currentPlayer().moveOutOfPenaltyBox();
+        currentPlayer.moveOutOfPenaltyBox();
     }
 
     private void askQuestion() {
-        log("The category is %s", board.getCurrentCategoryFor(currentPlayer()));
-        log("" + board.provideQuestionFor(currentPlayer()));
+        log("The category is %s", board.getCurrentCategoryFor(currentPlayer));
+        log("" + board.provideQuestionFor(currentPlayer));
     }
 }
