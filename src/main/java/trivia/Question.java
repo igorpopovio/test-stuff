0f7cd22a3ffa1796e3839b7193a6667763982d13
Igor Popov <igor.popov89@gmail.com>
2012-12-25 14:00:50 +0200

    Remove equals & hashCode for question
    Signed-off-by: Igor Popov <igor.popov89@gmail.com>

--- src/main/java/trivia/Question.java
+++ src/main/java/trivia/Question.java
@@ -28,26 +28,6 @@ public class Question {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Question question = (Question) o;
-
-        if (!category.equals(question.category)) return false;
-        if (!text.equals(question.text)) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = category.hashCode();
-        result = 31 * result + text.hashCode();
-        return result;
-    }
-
-    @Override
     public String toString() {
         return text;
     }
