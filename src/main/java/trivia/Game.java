ec9dacf1de3c5e86392fdff9c72c03610f7c8e68
Igor Popov <igor.popov89@gmail.com>
2012-12-26 11:18:17 +0200

    Prepare Game class for inheritance
    Signed-off-by: Igor Popov <igor.popov89@gmail.com>

--- src/main/java/trivia/Game.java
+++ src/main/java/trivia/Game.java
@@ -6,11 +6,18 @@ import java.util.List;
 import static trivia.Logger.log;
 
 public class Game {
-    private Board board;
-    private RingIterator<Player> players;
-    private Player currentPlayer;
-    private Die die;
-    private List<Player> penaltyBox;
+    protected Board board;
+    protected RingIterator<Player> players;
+    protected Player currentPlayer;
+    protected Die die;
+    protected List<Player> penaltyBox;
+
+    public Game(Game game) {
+        this.board = game.board;
+        this.players = game.players;
+        this.die = game.die;
+        this.penaltyBox = new ArrayList<>();
+    }
 
     public Game(Board board, List<Player> players, Die die) {
         this.board = board;
@@ -26,26 +33,26 @@ public class Game {
         } while (!isGameOver());
     }
 
-    public void nextPlayer() {
+    protected void nextPlayer() {
         currentPlayer = players.next();
         log("%s is the current player", currentPlayer);
     }
 
-    public void roll(int roll) {
+    protected void roll(int roll) {
         log("They have rolled a %d", roll);
         if (isInPenaltyBox()) doIfInPenaltyBox(roll);
         else doIfOutOfPenaltyBox(roll);
     }
 
-    public boolean isGameOver() {
+    protected boolean isGameOver() {
         return currentPlayer.getCoins() == 6;
     }
 
-    public boolean isInPenaltyBox() {
+    protected boolean isInPenaltyBox() {
         return penaltyBox.contains(currentPlayer);
     }
 
-    private void doIfInPenaltyBox(int roll) {
+    protected void doIfInPenaltyBox(int roll) {
         if (shouldKeepInPenaltyBox(roll)) {
             keepInPenaltyBox();
             return;
@@ -54,53 +61,53 @@ public class Game {
         doIfOutOfPenaltyBox(roll);
     }
 
-    private void doIfOutOfPenaltyBox(int roll) {
+    protected void doIfOutOfPenaltyBox(int roll) {
         updatePlace(roll);
         askQuestion();
     }
 
-    private boolean shouldKeepInPenaltyBox(int roll) {
+    protected boolean shouldKeepInPenaltyBox(int roll) {
         return roll % 2 == 0;
     }
 
-    private void keepInPenaltyBox() {
+    protected void keepInPenaltyBox() {
         log("%s is not getting out of the penalty box", currentPlayer);
     }
 
-    private void moveOutOfPenaltyBox() {
+    protected void moveOutOfPenaltyBox() {
         log("%s is getting out of the penalty box", currentPlayer);
         penaltyBox.remove(currentPlayer);
     }
 
-    private void updatePlace(int roll) {
+    protected void updatePlace(int roll) {
         board.updatePlace(currentPlayer, roll);
         log("%s's new location is %d", currentPlayer, board.getCurrentPlaceFor(currentPlayer));
     }
 
-    private void askQuestion() {
+    protected void askQuestion() {
         Question question = board.provideQuestionFor(currentPlayer);
         log("The category is %s", question.getCategory());
         log(question.getText());
         checkAnswer(question, currentPlayer.provideAnswerFor(question));
     }
 
-    private void checkAnswer(Question question, String answer) {
+    protected void checkAnswer(Question question, String answer) {
         if (question.isCorrectAnswer(answer)) doIfAnswerIsCorrect();
         else doIfAnswerIsWrong();
     }
 
-    private void doIfAnswerIsCorrect() {
+    protected void doIfAnswerIsCorrect() {
         currentPlayer.giveCoins(1);
         log("Answer was correct!!!!");
         log("%s now has %d Gold Coins.", currentPlayer, currentPlayer.getCoins());
     }
 
-    private void doIfAnswerIsWrong() {
+    protected void doIfAnswerIsWrong() {
         log("Question was incorrectly answered");
         moveInPenaltyBox();
     }
 
-    private void moveInPenaltyBox() {
+    protected void moveInPenaltyBox() {
         penaltyBox.add(currentPlayer);
         log("%s was sent to the penalty box", currentPlayer);
     }
